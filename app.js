002c7554e6da068d8d9c0a2a51febd1d48820498
Andy Seraphin <andy.seraphin@yahoo.com>
2021-05-21 17:55:17 -0400

    Updated with SQL

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
-const movieData = require("./webpages/javascript/movieData");
+const { client, syncAndSeed } = require("./webpages/javascript/movieData");
 const movieDetails = require("./webpages/javascript/movieDetails");
 const movieList = require("./webpages/javascript/movieList");
 const app = express();
@@ -8,18 +8,28 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/webpages"));
 
-app.get("/", (req, res) => {
-  const movies = movieData.list();
-  res.send(movieList(movies));
+app.get("/", async (req, res) => {
+  const movies = await client.query("SELECT * FROM Movie");
+  res.send(movieList(movies.rows));
 });
 
-app.get("/movies/:id", (req, res) => {
-  const movie = movieData.find(req.params.id);
-  res.send(movieDetails(movie));
+app.get("/movies/:id", async (req, res) => {
+  const movie = await client.query("SELECT * FROM Movie WHERE id=$1", [
+    req.params.id,
+  ]);
+  res.send(movieDetails(movie.rows[0]));
 });
 
-const PORT = 1337;
+const PORT = process.env.PORT || 1337;
 
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-});
+const setUp = async () => {
+  try {
+    await client.connect();
+    await syncAndSeed();
+    await app.listen(PORT, () => console.log(`App listening in port ${PORT}`));
+  } catch (ex) {
+    console.log(ex);
+  }
+};
+
+setUp();
--- webpages/javascript/movieData.js
+++ webpages/javascript/movieData.js
@@ -1,88 +1,32 @@
-const data = [
-  {
-    id: 1,
-    rotten: "94%",
-    title: "Nomadland",
-    director: "Chloe Zhao",
-    producers:
-      "Frances McDormand, Peter Spears, Mollye Asher, Dan Janvey and Chloé Zhao",
-    synopsis:
-      "A woman in her sixties who, after losing everything in the Great Recession, embarks on a journey through the American West, living as a van-dwelling modern-day nomad.",
-    result: "WINNER",
-  },
-  {
-    id: 2,
-    rotten: "98%",
-    title: "The Father",
-    director: "Florian Zeller",
-    producers: "David Parfitt, Jean-Louis Livi and Philippe Carcassonne",
-    synopsis:
-      "Anthony is 80, mischievous, living defiantly alone and rejecting the carers that his daughter, Anne, encouragingly introduces. Yet help is also becoming a necessity for Anne; she can't make daily visits anymore and Anthony's grip on reality is unravelling. As we experience the ebb and flow of his memory, how much of his own identity and past can Anthony cling to? How does Anne cope as she grieves the loss of her father, while he still lives and breathes before her? THE FATHER warmly embraces real life, through loving reflection upon the vibrant human condition; heart-breaking and uncompromisingly poignant - a movie that nestles in the truth of our own lives.",
-    result: "NOMINEES",
-  },
-  {
-    id: 3,
-    rotten: "96%",
-    title: "Judas and the Black Messiah",
-    director: "Shaka King",
-    producers: "Shaka King, Charles D. King and Ryan Coogler",
-    synopsis:
-      "Offered a plea deal by the FBI, William O'Neal infiltrates the Illinois chapter of the Black Panther Party to gather intelligence on Chairman Fred Hampton.",
-  },
-  {
-    id: 4,
-    rotten: "83%",
-    title: "Mank",
-    director: "David Fincher",
-    producers: "Ceán Chaffin, Eric Roth and Douglas Urbanski",
-    synopsis:
-      "1930s Hollywood is reevaluated through the eyes of scathing wit and alcoholic screenwriter Herman J. Mankiewicz as he races to finish 'Citizen Kane.'",
-  },
-  {
-    id: 5,
-    rotten: "98%",
-    title: "Minari",
-    director: "Lee Isaac Chung",
-    producers: "Christina Oh",
-    synopsis:
-      "A Korean American family moves to an Arkansas farm in search of its own American dream. Amidst the challenges of this new life in the strange and rugged Ozarks, they discover the undeniable resilience of family and what really makes a home.",
-  },
-  {
-    id: 6,
-    rotten: "90%",
-    title: "Promising Young Woman",
-    director: "Emerald Fennell",
-    producers: "Ben Browning, Ashley Fox, Emerald Fennell and Josey McNamara",
-    synopsis:
-      "Nothing in Cassie's life is what it appears to be -- she's wickedly smart, tantalizingly cunning, and she's living a secret double life by night. Now, an unexpected encounter is about to give Cassie a chance to right the wrongs from the past.",
-  },
-  {
-    id: 7,
-    rotten: "97%",
-    title: "Sound of Metal",
-    director: "Darius Marder",
-    producers: "Bert Hamelinck and Sacha Ben Harroche, Producers",
-    synopsis:
-      "A heavy-metal drummer's life is thrown into freefall when he begins to lose his hearing.",
-  },
-  {
-    id: 8,
-    rotten: "92%",
-    title: "The Trial of the Chicago 7",
-    director: "Aaron Sorkin",
-    producers: "Marc Platt and Stuart Besser",
-    synopsis:
-      "The film is based on the infamous 1969 trial of seven defendants charged by the federal government with conspiracy and more, arising from the countercultural protests in Chicago at the 1968 Democratic National Convention. The trial transfixed the nation and sparked a conversation about mayhem intended to undermine the U.S. government.",
-  },
-];
+const pg = require("pg");
 
-const list = () => {
-  return [...data];
+const client = new pg.Client("postgres://localhost/dealers_choice_db");
+
+const syncAndSeed = async () => {
+  const SQL = `
+    DROP TABLE IF EXISTS Movie;
+    CREATE TABLE Movie(
+      id INTEGER PRIMARY KEY,
+      rotten VARCHAR(100) NOT NULL,
+      title VARCHAR(254) UNIQUE NOT NULL,
+      director VARCHAR(100) NOT NULL,
+      producers TEXT NOT NULL,
+      synopsis TEXT NOT NULL,
+      result VARCHAR(100)
+    );
+    INSERT INTO Movie (id, rotten, title, director, producers, synopsis, result) VALUES(1,'94%','Nomadland', 'Chloe Zhao', 'Frances McDormand, Peter Spears, Mollye Asher, Dan Janvey and Chloé Zhao', 'A woman in her sixties who, after losing everything in the Great Recession, embarks on a journey through the American West, living as a van-dwelling modern-day nomad.', 'WINNER');
+    INSERT INTO Movie (id, rotten, title, director, producers, synopsis, result) VALUES(2,'98%','The Father','Florian Zeller','David Parfitt, Jean-Louis Livi and Philippe Carcassonne','Anthony is 80, mischievous, living defiantly alone and rejecting the carers that his daughter, Anne, encouragingly introduces. Yet help is also becoming a necessity for Anne; she cant make daily visits anymore and Anthonys grip on reality is unravelling. As we experience the ebb and flow of his memory, how much of his own identity and past can Anthony cling to? How does Anne cope as she grieves the loss of her father, while he still lives and breathes before her? THE FATHER warmly embraces real life, through loving reflection upon the vibrant human condition; heart-breaking and uncompromisingly poignant - a movie that nestles in the truth of our own lives.','NOMINEES');
+    INSERT INTO Movie (id, rotten, title, director, producers, synopsis, result) VALUES(3,'96%','Judas and the Black Messiah','Shaka King','Shaka King, Charles D. King and Ryan Coogler','Offered a plea deal by the FBI, William ONeal infiltrates the Illinois chapter of the Black Panther Party to gather intelligence on Chairman Fred Hampton.','');
+    INSERT INTO Movie (id, rotten, title, director, producers, synopsis, result) VALUES(4,'83%','Mank','David Fincher','Ceán Chaffin, Eric Roth and Douglas Urbanski','1930s Hollywood is reevaluated through the eyes of scathing wit and alcoholic screenwriter Herman J. Mankiewicz as he races to finish Citizen Kane.','');
+    INSERT INTO Movie (id, rotten, title, director, producers, synopsis, result) VALUES(5,'98%','Minari','Lee Isaac Chung','Christina Oh','A Korean American family moves to an Arkansas farm in search of its own American dream. Amidst the challenges of this new life in the strange and rugged Ozarks, they discover the undeniable resilience of family and what really makes a home.','');
+    INSERT INTO Movie (id, rotten, title, director, producers, synopsis, result) VALUES(6,'90%','Promising Young Woman','Emerald Fennell','Ben Browning, Ashley Fox, Emerald Fennell and Josey McNamara','Nothing in Cassies life is what it appears to be -- shes wickedly smart, tantalizingly cunning, and shes living a secret double life by night. Now, an unexpected encounter is about to give Cassie a chance to right the wrongs from the past.','');
+    INSERT INTO Movie (id, rotten, title, director, producers, synopsis, result) VALUES(7,'97%','Sound of Metal','Darius Marder','Bert Hamelinck and Sacha Ben Harroche, Producers','A heavy-metal drummers life is thrown into freefall when he begins to lose his hearing.','');
+    INSERT INTO Movie (id, rotten, title, director, producers, synopsis, result) VALUES(8,'92%','The Trial of the Chicago 7','Aaron Sorkin','Marc Platt and Stuart Besser','The film is based on the infamous 1969 trial of seven defendants charged by the federal government with conspiracy and more, arising from the countercultural protests in Chicago at the 1968 Democratic National Convention. The trial transfixed the nation and sparked a conversation about mayhem intended to undermine the U.S. government.','');
+  `;
+  await client.query(SQL);
 };
 
-const find = (id) => {
-  const movie = data.find((movie) => movie.id === +id);
-  return { ...movie };
+module.exports = {
+  client,
+  syncAndSeed,
 };
-
-module.exports = { list: list, find: find };
